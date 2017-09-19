1627cfaf7549bca2c449517fbc1440349381c847
ttany <460395142@qq.com>
2017-09-19 14:51:10 +0800

    Initial commit

---
+++ libs/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+#_*_coding:utf-8_*_
+# @Time    : 2017/9/13 11:14
+# @Author  : kebz
+# @Site    : 
+# @File    : __init__.py.py
---
+++ libs/sendsms.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+#_*_coding:utf-8_*_
+# @Time    : 2017/9/15 15:19
+# @Author  : kebz
+# @Site    : 
+# @File    : sendsms.py
+
